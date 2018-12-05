73182839a9d2349bccc0b1e28cf0ec91731ab3af
Phil Maurer <philmau@gmail.com>
2018-12-05 08:34:23 +0000

    Add bootstrap files

---
+++ views/gulpfile.js
@@ -0,0 +1,41 @@
+const server = require('browser-sync');
+const gulp = require('gulp');
+const scss = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const plumber = require('gulp-plumber');
+const config = require('./config.json');
+
+const src = './assets';
+const dist = './public';
+
+// Compile scss
+gulp.task('scss', function() {
+	gulp.src([`${src}/scss/app.scss`])
+		.pipe(plumber())
+		.pipe(scss())
+		.pipe(gulp.dest(`${dist}/css`))
+		.pipe(server.stream());
+});
+
+// Compile javascrip
+gulp.task('js', function() {
+	gulp.src([`${src}/js/main.js`])
+		.pipe(plumber())
+		.pipe(uglify())
+		.pipe(gulp.dest(`${dist}/js`))
+		.pipe(server.stream());
+});
+
+// Setup local server with injection
+gulp.task('serve', function() {
+	server.init({
+		proxy: 'localhost:' + config.port,
+		notify: false
+	});
+
+	gulp.watch(`${src}/js/**/*.js`, ['js']);
+	gulp.watch(`${src}/scss/**/*.scss`, ['scss']);
+});
+
+gulp.task('default', ['scss', 'bookmarklets']);
+gulp.task('dev', ['default', 'serve']);
