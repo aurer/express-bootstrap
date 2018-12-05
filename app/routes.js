601d67402a019dfcccdc34cd07048b7e82e2c410
Phil Maurer <philmau@gmail.com>
2018-12-05 12:00:28 +0000

    Improve title

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Welcome to Express' });
 });
 
 module.exports = router;
